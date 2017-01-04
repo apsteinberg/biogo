97c7c1c18f20f97dd6d26087ad8cd9b961ae6393
Mingzhi Lin <mingzhi9@gmail.com>
2017-01-04 18:32:43 -0500

    debug

--- seq/utils.go
+++ seq/utils.go
@@ -20,8 +20,35 @@ func Complement(s []byte) []byte {
 	m['G'] = 'C'
 	m['C'] = 'G'
 	bs := []byte{}
-	for i := 0; i < len(s); i++ {
-		bs = append(bs, m[s[i]])
+	for _, a := range s {
+		b := a
+		switch a {
+		case 'A':
+			b = 'T'
+			break
+		case 'a':
+			b = 't'
+			break
+		case 'T':
+			b = 'A'
+			break
+		case 't':
+			b = 'a'
+			break
+		case 'C':
+			b = 'G'
+			break
+		case 'c':
+			b = 'g'
+			break
+		case 'G':
+			b = 'C'
+			break
+		case 'g':
+			b = 'c'
+			break
+		}
+		bs = append(bs, b)
 	}
 
 	return bs
