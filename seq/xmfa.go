f86684122aa7b9a41e5f8b52fb0f81877b1586f0
Mingzhi Lin <mingzhi9@gmail.com>
2017-01-05 11:24:32 -0500

    add xmfa reader

--- seq/xmfa.go
+++ seq/xmfa.go
@@ -7,6 +7,58 @@ import (
 	"os"
 )
 
+// XMFAReader for extended Multi-FASTA format
+type XMFAReader struct {
+	r *bufio.Reader
+}
+
+// NewXMFAReader return a XMFAReader.
+func NewXMFAReader(rd io.Reader) *XMFAReader {
+	xmfa := XMFAReader{}
+	xmfa.r = bufio.NewReader(rd)
+	return &xmfa
+}
+
+// Read returns a alignment (block).
+func (r XMFAReader) Read() (alignment []Sequence, err error) {
+	var seqID, seqBytes []byte
+	for {
+		var line []byte
+		line, err = r.r.ReadBytes('\n')
+		if err != nil {
+			return
+		}
+
+		line = bytes.TrimSpace(line)
+		if line[0] == '=' {
+			break
+		} else if line[0] == '#' {
+			continue
+		} else if line[0] == '>' {
+			if len(seqBytes) > 0 {
+				ss := Sequence{}
+				ss.Id = string(seqID)
+				ss.Seq = seqBytes
+				alignment = append(alignment, ss)
+			}
+			seqID = line[1:]
+			seqBytes = []byte{}
+		} else {
+			seqBytes = append(seqBytes, line...)
+		}
+
+	}
+
+	if len(seqBytes) > 0 {
+		ss := Sequence{}
+		ss.Id = string(seqID)
+		ss.Seq = seqBytes
+		alignment = append(alignment, ss)
+	}
+
+	return
+}
+
 // ReadXMFA read sequences from XMFA file.
 func ReadXMFA(filename string) [][]*Sequence {
 	seqGroups := [][]*Sequence{}
